728c20f1e986ab0aa7082f403ff53bf43092a870
Mlodzieniak <116485218+Mlodzieniak@users.noreply.github.com>
2023-04-27 11:43:45 +0200

    signIn function. Able to return user data from firebase

--- src/PrivateRoute.jsx
+++ src/PrivateRoute.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable max-len */
 /* eslint-disable react/prop-types */
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "./Auth";
 
 function PrivateRoute({ children }) {
   const { currentUser } = useContext(AuthContext);
+  useEffect(() => {
+    console.log(currentUser);
+  }, []);
   return (
     // <Route
     //   {...rest}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@mui/material";
 import "../styles/login.css";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChange = (event, cb) => {
+    cb(event.target.value);
+  };
+  // const submit = () => {
+  //   signInWithEmailAndPassword(auth, email, password);
+  // };
+
+  const signIn = async () => {
+    try {
+      const userCredentials = await signInWithEmailAndPassword(auth, email, password);
+      const { user } = userCredentials;
+      console.log(user);
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(`${errorCode}: ${errorMessage}`);
+    }
+  };
   return (
     <div className="login-page">
       <div className="logo-wrapper">
@@ -10,9 +33,9 @@ function Login() {
         <div className="motto">Connect with friends and world around you on Greenbook.</div>
       </div>
       <div className="login-wrapper">
-        <input type="text" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <Button variant="contained">Log In</Button>
+        <input type="text" placeholder="Email" onChange={(event) => onChange(event, setEmail)} />
+        <input type="password" placeholder="Password" onChange={(event) => onChange(event, setPassword)} />
+        <Button variant="contained" onClick={signIn}>Log In</Button>
 
         <span className="divider" />
         <Button variant="contained" color="secondary">Sign in with Google</Button>
