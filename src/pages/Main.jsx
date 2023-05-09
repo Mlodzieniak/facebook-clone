902dca090d324604152f1934eca78d784a97f6fd
Mlodzieniak <116485218+Mlodzieniak@users.noreply.github.com>
2023-05-09 13:40:19 +0200

    uploading eventsto db

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,38 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Form } from "react-router-dom";
 import { Button, TextField } from "@mui/material";
-import { ref, uploadBytes } from "firebase/storage";
-import { storage } from "../firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { doc, setDoc, Timestamp } from "firebase/firestore";
+import { uuidv4 } from "@firebase/util";
+import { storage, db } from "../firebase";
+import { AuthContext } from "../Auth";
 
 function Main() {
-  const [comment, setComment] = useState("");
-  const [file, setFile] = useState("");
+  const [text, setText] = useState("");
+  const [file, setFile] = useState(null);
   const [imageRef, setImageRef] = useState(null);
 
-  // const [formData, setFormData] = useState({});
-  const handleCommentChange = (event) => {
-    setComment(event.target.value);
+  const currentUser = useContext(AuthContext);
+  const { uid } = currentUser;
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
   };
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const snapshot = await uploadBytes(imageRef, file);
-    console.log(snapshot);
-  };
-  const handleFileUpdate = (event) => {
-    setFile(event.target.files[0]);
-    setImageRef(ref(storage, `events/${file.lastModified.toString()}`));
+    await uploadBytes(imageRef, file);
+    const url = await getDownloadURL(imageRef);
+    const eventData = {
+      creationDate: Timestamp.fromDate(new Date()),
+      text,
+      authorId: uid,
+      comments: [],
+      imageURL: url,
+    };
+    setDoc(doc(db, "events", uuidv4()), eventData);
   };
 
+  useEffect(() => {
+    if (file) {
+      setImageRef(ref(storage, `events/${file.name}`));
+    }
+  }, [file]);
   return (
     <div className="main-page">
       <Form method="post" className="create-event">
+        {/* {imageURL ? (<img src={imageURL} alt={file.name} />) : null} */}
         <TextField
-          value={comment}
-          onChange={handleCommentChange}
+          value={text}
+          onChange={handleTextChange}
           aria-label="comment"
           placeholder="Write a comment..."
         />
-        <input type="file" accept="image/*" name="file-loader" id="file-loader" onChange={handleFileUpdate} />
+        <input type="file" accept="image/*" name="file-loader" id="file-loader" onChange={(event) => { setFile(event.target.files[0]); }} />
         <Button type="submit" variant="contained" onClick={handleSubmit}>
           Post
         </Button>
