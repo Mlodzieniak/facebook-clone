ae85da840caacf17905cfd54abbade98955af115
Mlodzieniak <116485218+Mlodzieniak@users.noreply.github.com>
2023-05-09 11:40:28 +0200

    uploads images to firestorage

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,13 +1,26 @@
 import React, { useState } from "react";
 import { Form } from "react-router-dom";
 import { Button, TextField } from "@mui/material";
-// import { db } from "../firebase";
+import { ref, uploadBytes } from "firebase/storage";
+import { storage } from "../firebase";
 
 function Main() {
   const [comment, setComment] = useState("");
+  const [file, setFile] = useState("");
+  const [imageRef, setImageRef] = useState(null);
+
   // const [formData, setFormData] = useState({});
   const handleCommentChange = (event) => {
     setComment(event.target.value);
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const snapshot = await uploadBytes(imageRef, file);
+    console.log(snapshot);
+  };
+  const handleFileUpdate = (event) => {
+    setFile(event.target.files[0]);
+    setImageRef(ref(storage, `events/${file.lastModified.toString()}`));
   };
 
   return (
@@ -19,8 +32,8 @@ function Main() {
           aria-label="comment"
           placeholder="Write a comment..."
         />
-        <input type="file" name="file-loader" id="file-loader" />
-        <Button type="submit" variant="contained">
+        <input type="file" accept="image/*" name="file-loader" id="file-loader" onChange={handleFileUpdate} />
+        <Button type="submit" variant="contained" onClick={handleSubmit}>
           Post
         </Button>
       </Form>
